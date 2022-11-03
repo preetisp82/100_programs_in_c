460cb95f967d89b1a4721fa1c02c6982b250cbd6
preetisp82 <110542915+preetisp82@users.noreply.github.com>
2022-11-03 14:35:46 +0530

    Create ASCII_value_of_a_charector.c

---
+++ ASCII_value_of_a_charector.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {  
+
+    char c;
+
+    printf("Enter a character: ");
+
+    scanf("%c", &c);  
+
+    printf("ASCII value of %c = %d", c, c);
+
+    
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+    
+
+
+
+
+
